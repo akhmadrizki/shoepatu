88b7a84a9802fc810330e98eced2604fb703094a
akhmadrizki <akhmadrizki24@gmail.com>
2023-09-10 19:07:48 +0800

    chore: adding banner

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-banner": "linear-gradient(180deg, #FFF3F0 0%, #FFEFEC 100%)",
+      },
+      colors: {
+        "gradient-banner": "#FFF3F0",
+      },
+      scale: {
+        "-100": "-1",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
