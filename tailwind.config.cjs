9bf27ff4acf14d7712c904ce9f2d8f1336ac3a21
akhmadrizki <akhmadrizki24@gmail.com>
2023-09-15 15:02:48 +0800

    chore: responsive menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "375px",
+      },
       backgroundImage: {
         "gradient-banner": "linear-gradient(180deg, #FFF3F0 0%, #FFEFEC 100%)",
       },
